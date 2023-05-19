ebae6407f4224a9030902caacb541311bb312f03
Jinyu <cadenho@hotmail.com>
2023-05-19 13:24:25 +0800

    feat: add d.ts

---
+++ typings/app.d.ts
@@ -0,0 +1,5 @@
+import BulletScreen from './bulletScreen';
+import StyledBullet from './styleBullet';
+import useBulletScreen from './hooks/useBulletScreen';
+export default BulletScreen;
+export { StyledBullet, useBulletScreen };
